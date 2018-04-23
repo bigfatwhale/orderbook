041e8ddb16c261865c218427648efbe48874f74c
bigfatwhale <bigfatwhale@gmail.com>
2018-04-23 22:27:25 +0800

    Add van Emde Boas data structure

--- tests/test_orderbook.cpp
+++ tests/test_orderbook.cpp
@@ -9,8 +9,13 @@
 #include <memory>
 #include <string>
 #include <fstream>
+#include <iostream>
 #include <boost/test/unit_test.hpp>
+#include <boost/dynamic_bitset.hpp>
 #include "OrderBook.h"
+#include "veb.h"
+
+using namespace std;
 
 BOOST_AUTO_TEST_SUITE( test_parse_suite )
 
@@ -28,6 +33,19 @@ BOOST_AUTO_TEST_SUITE( test_parse_suite )
         b.removeOrder(o);
         BOOST_TEST( b.volumeForPricePoint(price, o.side) == 0 );
 
+    }
+
+    BOOST_AUTO_TEST_CASE( test_veb )
+    {
+//        int n = 10000;
+//        boost::dynamic_bitset<> x(n);
+//        x[100] = 1;
+//
+//        cout << x[99] << x[100] << endl;
+
+        veb v{18};
+        int k = 200;
+        //cout << v.m_clusters.size() << endl;
     }
 
 BOOST_AUTO_TEST_SUITE_END()
---
+++ veb.cpp
@@ -0,0 +1,86 @@
+//
+// Created by Chu Ming on 4/22/18.
+//
+
+#include <iostream>
+#include <algorithm>
+#include <boost/dynamic_bitset.hpp>
+#include "veb.h"
+
+using namespace std;
+
+veb::veb(int numBits) : m_numBits{numBits},
+                        m_min{-1},
+                        m_max{-1},
+                        m_lsb{m_numBits / 2},
+                        m_msb{m_lsb + (m_numBits % 2)},
+                        m_cluster_size{1 << m_msb}
+{
+    //cout << "creating veb(" << m_numBits << "), msb=" << m_msb << " lsb=" << m_lsb << endl;
+
+    // usr(u) : upper square root = 2^( ceil( ( lg u / 2 ) ) )
+    // lsr(u) : lower square root = 2^( floor( ( lg u / 2 ) ) )
+    if (m_numBits > 1)
+    {
+        // initialize recursive data structure.
+        m_cluster = new veb*[ m_cluster_size ];
+        for (int i=0; i<m_cluster_size; i++)
+            m_cluster[i] = new veb(m_lsb);
+        m_summary = new veb(m_msb);
+    }
+
+}
+
+veb::~veb()
+{
+    if (m_cluster != nullptr)
+    {
+        for (int i = 0; i < m_cluster_size; i++)
+            delete m_cluster[i];
+        delete[] m_cluster;
+        delete m_summary;
+    }
+}
+
+inline int veb::high(int x)
+{
+    // high(x) = floor( x / lsr(u) )
+    return x >> m_lsb;
+}
+
+inline int veb::low(int x)
+{
+    return x & ( 1 << ( m_lsb + 1 ) - 1 );
+}
+
+inline int veb::index(int i, int j)
+{
+    return ( i << m_lsb ) + j;
+}
+
+inline void veb::empty_insert(veb &v, int x)
+{
+    v.m_min = v.m_max = x;
+}
+
+void veb::insert(int x)
+{
+    if ( m_min == -1 ) // m_min == -1 means current veb structure is empty
+        m_min = m_max = x; // inserting x just means setting its value the min and max element
+    else
+    {
+        if (x < m_min)
+            swap( x, m_min );
+        if (m_numBits > 1)
+        {
+            if ( m_cluster[ high(x) ]->m_min == -1 )
+            {
+                m_summary->insert( high(x) ); // mark dirty in summary bit vector
+                empty_insert( *m_cluster[ high(x) ], low(x) );
+            } else
+                m_cluster[ high(x) ]->insert( low(x) );
+        }
+        if (x > m_max)
+            m_max = x;
+    }
+}
