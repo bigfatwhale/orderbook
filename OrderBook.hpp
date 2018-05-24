6644cc25ef3fc7e369b919093b4472d809ef4bec
Pix <bigfatwhale@gmail.com>
2018-05-24 11:45:31 +0800

    Refactor addOrder method to make it neater.

--- OrderBook.hpp
+++ OrderBook.hpp
@@ -98,7 +98,25 @@ class LimitOrderBook {
     // class implementing the facilities for a "continuous double auction"
     // see https://arxiv.org/abs/1012.0349 for general survey of limit order books.
 public:
+
+    using IBookType = Book<PriceBucketManagerT>;
+
     LimitOrderBook() : m_buyBook{BookType::BUY}, m_sellBook{BookType::SELL} {}
+
+    void doCrossSpread(Order &order, IBookType &book)
+    {
+        int32_t residual_volume;
+        // iterate and walk through the prices, generating filled order msgs.
+        bool price_condition = order.side == BookType::BUY ?
+                                    order.price >= book.bestPrice() :
+                                    order.price <= book.bestPrice();
+
+        if ( ( book.bestPrice() > 0 ) && price_condition )
+        {
+            residual_volume = crossSpreadWalk(order, book);
+            order.volume = residual_volume;
+        }
+    }
 
     void addOrder(Order &order)
     {
@@ -106,36 +124,23 @@ public:
         // for some particular stock is $100.00 and the ask is $100.10, and then some one puts
         // in a new order with bid at $100.10, then we have to match this with the sell book
         // and generate an execution msg.
-        int32_t residual_volume;
+
         if ( order.side == BookType::BUY )
         {
-            // iterate and walk through the prices, generating filled order msgs.
-            if ( ( m_sellBook.bestPrice() > 0 ) && ( order.price >= m_sellBook.bestPrice() ) )
-            {
-                residual_volume = crossSpreadWalk(order, m_sellBook);
+            doCrossSpread(order, m_sellBook);
 
-                if (residual_volume > 0)
-                    order.volume = residual_volume;
-                else
-                    return;
-            }
-
-            m_buyBook.addOrder(order);
+            // if order.volume is still +ve, the can be either there is no cross-spread walk done
+            // or the cross-spread walk only filled part of the volume. In that case we continue to
+            // add the left-over volume in a new order.
+            if (order.volume > 0)
+                m_buyBook.addOrder(order);
         }
         else
         {
-            // iterate and walk through the prices, generating filled order msgs.
-            if ( ( m_buyBook.bestPrice() > 0 ) && ( order.price <= m_buyBook.bestPrice() ) )
-            {
-                residual_volume = crossSpreadWalk(order, m_buyBook);
-
-                if (residual_volume > 0)
-                    order.volume = residual_volume;
-                else
-                    return;
-            }
-
-            m_sellBook.addOrder(order);
+            doCrossSpread(order, m_buyBook);
+            // see explanation for if-clause.
+            if (order.volume > 0)
+                m_sellBook.addOrder(order);
         }
     }
 
@@ -168,8 +173,6 @@ public:
     // for unittest convenience only
     void addOrderToBuyBook(  Order &order ) { m_buyBook.addOrder(order); }
     void addOrderToSellBook( Order &order ) { m_sellBook.addOrder(order); }
-
-    using IBookType = Book<PriceBucketManagerT>;
 
 private:
 
