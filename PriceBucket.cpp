059dfdbad571d2f59a72a6cdca6b7bfbe933a2c2
bigfatwhale <bigfatwhale@gmail.com>
2018-05-25 22:30:13 +0800

    Convert tabs to spaces

--- PriceBucket.cpp
+++ PriceBucket.cpp
@@ -12,27 +12,27 @@ PriceBucket::PriceBucket(uint64_t pricePoint) : m_pricePoint{pricePoint} {}
 
 void PriceBucket::addOrder(Order const &order)
 {
-    m_orders.push_back(order);
+	m_orders.push_back(order);
 }
 
 void PriceBucket::removeOrder(Order const &order)
 {
-    auto f = [&order](Order &o1) { return o1.orderId == order.orderId; };
-    auto it = std::find_if(m_orders.begin(), m_orders.end(), f);
-    if (it != m_orders.end())
-        m_orders.erase(it);
+	auto f = [&order](Order &o1) { return o1.orderId == order.orderId; };
+	auto it = std::find_if(m_orders.begin(), m_orders.end(), f);
+	if (it != m_orders.end())
+		m_orders.erase(it);
 }
 
 uint32_t PriceBucket::numOrders()
 {
-    return m_orders.size();
+	return m_orders.size();
 }
 
 uint32_t PriceBucket::totalVolume()
 {
-    // count the total volume we have in this bucket.
-    auto f = []( int a, Order const& b)
-               { return a + b.volume; };
-    auto sum = accumulate( m_orders.begin(), m_orders.end(), 0, f);
-    return sum;
+	// count the total volume we have in this bucket.
+	auto f = []( int a, Order const& b)
+			   { return a + b.volume; };
+	auto sum = accumulate( m_orders.begin(), m_orders.end(), 0, f);
+	return sum;
 }
--- vebBucketSet.hpp
+++ vebBucketSet.hpp
@@ -8,37 +8,37 @@
 template< typename PriceBucketT = PriceBucket, int numBits=20 >
 class vebBucketSet
 {
-	using MapType = std::unordered_map<uint64_t, std::shared_ptr<PriceBucketT>>;
+    using MapType = std::unordered_map<uint64_t, std::shared_ptr<PriceBucketT>>;
 public:
-	vebBucketSet() : m_veb{numBits} {}
+    vebBucketSet() : m_veb{numBits} {}
 
     std::shared_ptr<PriceBucketT> find( uint64_t price )
     { 
-    	if (m_veb.isMember(price))
-    	{
-    		auto it = m_map.find(price);
-    		return it->second;
-    	}
-    	else
-    		return nullptr;
-	}
+        if (m_veb.isMember(price))
+        {
+            auto it = m_map.find(price);
+            return it->second;
+        }
+        else
+            return nullptr;
+    }
 
     std::shared_ptr<PriceBucketT> successor( uint64_t price )
     {
-    	auto successor = m_veb.successor(price);
-    	if ( successor == -1 )
-    		return nullptr;
+        auto successor = m_veb.successor(price);
+        if ( successor == -1 )
+            return nullptr;
 
-    	return m_map.find(successor)->second;
+        return m_map.find(successor)->second;
     }
 
     std::shared_ptr<PriceBucketT> predecessor( uint64_t price )
     {
-    	auto predecessor = m_veb.predecessor(price);
-    	if ( predecessor == -1 )
-    		return nullptr;
+        auto predecessor = m_veb.predecessor(price);
+        if ( predecessor == -1 )
+            return nullptr;
 
-    	return m_map.find(predecessor)->second;
+        return m_map.find(predecessor)->second;
     }
 
     std::shared_ptr<PriceBucketT> insert( typename MapType::value_type keyValPair )
@@ -48,24 +48,24 @@ public:
         auto [item, ok] = m_map.insert( keyValPair );
         if (ok)
         {
-        	m_veb.insert(keyValPair.first);
-        	return item->second;
+            m_veb.insert(keyValPair.first);
+            return item->second;
         }
         else
-        	return std::shared_ptr<PriceBucketT>();
+            return std::shared_ptr<PriceBucketT>();
     }
 
-	void remove( uint64_t price ) 
-	{
-		m_veb.remove(price);  
-		m_map.erase(price); 
-	}
+    void remove( uint64_t price ) 
+    {
+        m_veb.remove(price);  
+        m_map.erase(price); 
+    }
     
     uint64_t minPrice()
-	{
-	    auto i = m_veb.findMin();
-		return i == -1 ? 0 : i;
-	}
+    {
+        auto i = m_veb.findMin();
+        return i == -1 ? 0 : i;
+    }
     uint64_t maxPrice()
     {
         auto i = m_veb.findMax();
@@ -73,8 +73,8 @@ public:
     }
 
 private:
-	veb m_veb;
-	MapType m_map;
+    veb m_veb;
+    MapType m_map;
 };
 
 #endif
