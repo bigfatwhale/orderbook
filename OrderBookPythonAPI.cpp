e0a829560e037b7fe6625d1a86ae604f3cc47335
Pix <bigfatwhale@gmail.com>
2018-06-08 21:17:40 +0800

    Export PriceBucket to python and also add first simple python tests.

--- OrderBookPythonAPI.cpp
+++ OrderBookPythonAPI.cpp
@@ -2,13 +2,14 @@
 #include "Python.h"
 #include "Order.h"
 #include "OrderBook.hpp"
+#include "PriceBucket.h"
 #include <ostream>
 #include <string>
 #include <boost/python.hpp>
 
 using namespace boost::python;
 
-void export_orderbook_to_python()
+void export_orderbook()
 {
 
 	class_< LimitOrderBook<> >("LimitOrderBook")
@@ -19,15 +20,31 @@ void export_orderbook_to_python()
 		.def("bestBid",             &LimitOrderBook<>::bestBid )
 		.def("bestAsk",             &LimitOrderBook<>::bestAsk )
 		.add_property("bids", range(&LimitOrderBook<>::bids_begin, &LimitOrderBook<>::bids_end))
+		.add_property("asks", range(&LimitOrderBook<>::asks_begin, &LimitOrderBook<>::asks_end))
 		;
 
+}
+
+void export_booktype()
+{
 	enum_<BookType>("BookType")
         .value("BUY",  BookType::BUY)
         .value("SELL", BookType::SELL)
         ;
 }
 
-void export_order_to_python()
+void export_price_bucket()
+{
+	class_<PriceBucket>("PriceBucket")
+		.def(init<>())
+		.def(init<uint64_t>())
+		.def("__iter__", range(&PriceBucket::begin, &PriceBucket::end))
+		.def_readwrite("pricePoint", &PriceBucket::m_pricePoint)
+		;
+}
+
+
+void export_order()
 {
 	class_<Order>("Order")
 		.def(init<>())
@@ -44,7 +61,8 @@ BOOST_PYTHON_MODULE(orderbook_api)
 {
     PyEval_InitThreads(); // need this to mess around with GIL
 
-	export_orderbook_to_python();
-	export_order_to_python();
-
+	export_orderbook();
+	export_order();
+	export_booktype();
+	export_price_bucket();
 }
---
+++ tests/test_orderbook_api.py
@@ -0,0 +1,26 @@
+
+from orderbook_api import *
+import unittest
+
+
+class ParserTest( unittest.TestCase ):
+
+    def test_simple(self):
+
+		lob = LimitOrderBook()
+
+		o = Order(1000, 3222, 4444, BookType.BUY, '1341')
+
+		lob.addOrder(o)
+
+		bucket_iter  = iter(lob.bids)
+		price_bucket = bucket_iter.next()
+
+		self.assertEqual( price_bucket.pricePoint, 3222)
+
+		orderIter = iter(price_bucket)
+		order = orderIter.next()
+		self.assertEqual( order.orderId, 1000)
+
+if __name__=='__main__':
+    unittest.main()
