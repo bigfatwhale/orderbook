36a24e1302a462319e5c3c871dab46e6330a1267
bigfatwhale <bigfatwhale@gmail.com>
2018-06-13 12:19:33 +0800

    Implement add order for Book struct

--- src/orderbook.rs
+++ src/orderbook.rs
@@ -3,57 +3,82 @@ use std::collections::BTreeMap;
 
 #[derive(Clone)]
 pub struct Order {
-	pub order_id : u64, 
-	pub price    : u64, 
-	pub volume   : u32, 
-	pub side     : i8, 
-	pub part_id  : String, 
+    pub order_id : u64, 
+    pub price    : u64, 
+    pub volume   : u32, 
+    pub side     : i8, 
+    pub part_id  : String, 
 }
 
 pub struct PriceBucket {
 
-	pub price_level : u64, 
-	orders      : Vec<Order>, 
+    pub price_level : u64, 
+    orders      : Vec<Order>, 
 
 }
 
 pub struct Book {
 
-	price_buckets : BTreeMap<u64, PriceBucket>
+    price_buckets : BTreeMap<u64, PriceBucket>
 
 }
 
 pub trait OrderManager {
 
-	fn add_order( &mut self, order : Order );
-	fn remove_order( &mut self, order : Order );
+    fn add_order( &mut self, order : Order );
+    fn remove_order( &mut self, order : Order );
 
 }
 
 impl OrderManager for PriceBucket {
 
-	fn add_order( &mut self, order : Order ) {
-		self.orders.push(order);
-	}
+    fn add_order( &mut self, order : Order ) {
+        self.orders.push(order);
+    }
 
-	fn remove_order( &mut self, order : Order ) {
-		// just do linear scan for now, worry about performance later.
-		let idx  = self.orders.iter().position(|x| x.order_id == order.order_id );
-		if idx.is_some() {
-			self.orders.remove(idx.unwrap());
-		}
-	}
+    fn remove_order( &mut self, order : Order ) {
+        // just do linear scan for now, worry about performance later.
+        let idx  = self.orders.iter().position(|x| x.order_id == order.order_id );
+        if idx.is_some() {
+            self.orders.remove(idx.unwrap());
+        }
+    }
 }
 
 impl PriceBucket {
 
-	pub fn new(price_level : u64) -> PriceBucket {
-		PriceBucket{ price_level : price_level, orders : Vec::new() }
-	} 
+    pub fn new(price_level : u64) -> PriceBucket {
+        PriceBucket{ price_level : price_level, orders : Vec::new() }
+    } 
 
-	pub fn volume(&self) -> u32 {
-		self.orders.iter().map(|x| x.volume ).sum()
-	} 
+    pub fn volume(&self) -> u32 {
+        self.orders.iter().map(|x| x.volume ).sum()
+    } 
 }
 
+impl OrderManager for Book {
 
+    fn add_order( &mut self, order : Order ) {
+        if !self.price_buckets.contains_key(&order.price) {
+            let price = order.price;
+            let mut price_bucket = PriceBucket::new(price);
+            price_bucket.add_order(order);
+            self.price_buckets.insert( price, price_bucket );
+        } else {
+
+            let mut opt_bucket = self.price_buckets.get(&order.price);
+            if opt_bucket.is_some() {
+                opt_bucket.unwrap().add_order(order);
+            }
+            
+            
+            
+        }
+
+        
+    }
+
+    fn remove_order( &mut self, order : Order ) {
+
+    }
+}
