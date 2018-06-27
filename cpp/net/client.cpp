1d9a64cf3ed9e410111c21f700d4726aa796be2b
bigfatwhale <bigfatwhale@gmail.com>
2018-06-27 21:11:21 +0800

    Add basic logic for client code.

--- cpp/net/client.cpp
+++ cpp/net/client.cpp
@@ -1,9 +1,11 @@
 
 #include <boost/asio.hpp>
 #include "client.h"
+#include "protocol.h"
 
 using namespace boost::asio;
 using namespace boost::asio::ip;
+using namespace boost::system;
 
 OrderBookClient::OrderBookClient(io_context &io_context, 
                                  const tcp::resolver::results_type& endpoints) : 
@@ -15,17 +17,57 @@ OrderBookClient::OrderBookClient(io_context &io_context,
 void OrderBookClient::do_connect(const tcp::resolver::results_type& endpoints)
 {
     async_connect(m_socket, endpoints, 
-        [this](boost::system::error_code ec, tcp::endpoint)
+        [this](error_code ec, tcp::endpoint)
         {
             if (!ec)
             {
-                do_read_session_id();
+                read_header();
             }
         }
     );
 }
 
-void OrderBookClient::do_read_session_id()
+void OrderBookClient::read_header()
 {
 
+    async_read(m_socket, buffer(m_buffer, protocol::header_length),
+        [this]( error_code ec, std::size_t)
+        {
+            if (!ec)
+            {
+                uint16_t msg_type = *reinterpret_cast<uint16_t *>(m_buffer);
+                uint16_t msg_len  = *reinterpret_cast<uint16_t *>(m_buffer + sizeof(uint16_t));
+                read_body(msg_type, msg_len);
+            }
+            else
+            {
+                m_socket.close();
+            }
+
+        }
+    );
+}
+
+void OrderBookClient::read_body(uint16_t msg_type, uint16_t msg_len)
+{
+    async_read(m_socket, buffer(m_buffer, msg_len),
+        [this, msg_type]( error_code ec, std::size_t)
+        {
+            if (!ec)
+            {
+                switch(msg_type)
+                {
+                    case protocol::message_type::CLIENT_ID:
+                        m_client_id = *reinterpret_cast<uint32_t *>(m_buffer);
+                        break;
+                }
+                read_header();
+            }
+            else
+            {
+                m_socket.close();
+            }
+
+        }
+    );
 }
--- cpp/net/client.h
+++ cpp/net/client.h
@@ -12,10 +12,15 @@ public:
     OrderBookClient(boost::asio::io_context &io_context, 
                     const boost::asio::ip::tcp::resolver::results_type& endpoints);
     void do_connect(const boost::asio::ip::tcp::resolver::results_type& endpoints);
-    void do_read_session_id();
+    void read_header();
+    void read_body(uint16_t msg_type, uint16_t msg_len);
 private:
     boost::asio::io_context &m_io_context;
     boost::asio::ip::tcp::socket m_socket;
+
+    char m_buffer[1024];
+    uint32_t m_client_id;
+    
 };
 
 
